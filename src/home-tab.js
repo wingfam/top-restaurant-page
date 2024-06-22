4c877f52976ce9a06ecbdf74afdc11be56cda459
Nguyen Huynh Nhat Minh <66111145+wingfam@users.noreply.github.com>
2024-06-22 10:35:36 +0700

    added some styling to home-tab and logo to header

--- src/home-tab.js
+++ src/home-tab.js
@@ -5,12 +5,14 @@ const HomeTab = function() {
     const slogan = document.createElement("h2");
     const homeImg = document.createElement("img");
     const description = document.createElement("p");
+    const textBox = document.createElement("div");
     // const imageCredit = document.createElement("p");
 
-    homeDiv.setAttribute("class", "home-div");
+    homeDiv.setAttribute("class", "home-tab");
     slogan.setAttribute("class", "slogan");
     homeImg.setAttribute("class", "home-image");
     description.setAttribute("class", "description");
+    textBox.setAttribute("class", "text-box");
     // imageCredit.setAttribute("class", "image-credit");
 
     slogan.textContent = "Best Vietnamese Noodle in Town!";
@@ -18,9 +20,10 @@ const HomeTab = function() {
     description.textContent = "This restaurant is not a real restaurant, but I bet you it can be the best Vietnames Noodle restaurant in the world!";
     // imageCredit.textContent = "Photo by Vincent Rivaud on pexels.com";
 
-    homeDiv.appendChild(slogan);
+    textBox.appendChild(slogan);
+    textBox.appendChild(description);
     homeDiv.appendChild(homeImg);
-    homeDiv.appendChild(description);
+    homeDiv.appendChild(textBox);
     // homeDiv.appendChild(imageCredit);
 
     return homeDiv;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import HomeTab from "./home-tab";
-import Logo from './restaurant-logo.jpg';
+import LogoScr from './restaurant-logo.jpg';
 
 import './reset.css';
 import './style.css';
 
+const header = document.querySelector("header");
+const nav = document.querySelector("nav");
 const content = document.querySelector("#content");
 const home = HomeTab();
 
+const logoImage = document.createElement("img");
+
+logoImage.src = LogoScr;
+
+logoImage.setAttribute("class", "logo-img");
+
+header.insertBefore(logoImage, nav);
 content.appendChild(home);
