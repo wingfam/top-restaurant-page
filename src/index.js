55e4f26dd9f3d0a9e5bec2d1f1840bab28c60cfa
Nguyen Huynh Nhat Minh <66111145+wingfam@users.noreply.github.com>
2024-06-24 11:54:53 +0700

    removed logo image

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import HomeTab from "./home-tab";
-import LogoScr from './restaurant-logo.jpg';
 
 import './reset.css';
 import './style.css';
 
-const header = document.querySelector("header");
-const nav = document.querySelector("nav");
 const content = document.querySelector("#content");
 const home = HomeTab();
 
-const logoImage = document.createElement("img");
-
-logoImage.src = LogoScr;
-
-logoImage.setAttribute("class", "logo-img");
-
-header.insertBefore(logoImage, nav);
 content.appendChild(home);
